42fd4d3e170d66b5942cfb3bad68dec82c78b0fa
Sebastian Rataj <100698720+SRataj1005@users.noreply.github.com>
2022-06-07 12:42:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) 
+  {
+Scanner nowy = new Scanner(System.in);
+System.out.println("Podaj liczbę: ");
+int a = nowy.nextInt();
+    if (a > 0)
+    {
+      System.out.println("Liczba jest dodatnia");
+    }
+    else
+    {
+      System.out.println("Liczba jest ujemna");
+    }
+
+
+  
+    
+  }
+}
